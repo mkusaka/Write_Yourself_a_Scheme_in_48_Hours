8c120896caa55cec366f90067bef836a9903434c
mkusaka <hinoshita1992@gmail.com>
2019-03-12 00:44:09 +0900

    not so bad answer to exercise 2 :cry:

--- section1/exercise2.hs
+++ section1/exercise2.hs
@@ -4,10 +4,4 @@ import System.Environment
 main::IO()
 main = do
   args <- getArgs
-  x <- args !! 0
-  y <- args !! 1
-  numx <- read x
-  numy <- read y
-  result <- x ** y
-  strResult <- show result
-  putStrLn("answer is,,, " ++ strResult ++ " !!")
+  putStrLn("answer is,,, " ++ show ( read (args !! 0) ** read(args !! 1)) ++ " !!")
