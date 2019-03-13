9d49f18ffb9ee68e1b3c4b50734ae1f500b21231
mkusaka <hinoshita1992@gmail.com>
2019-03-13 23:43:38 +0900

    section2

---
+++ section2/exercise21.hs
@@ -0,0 +1,63 @@
+module Main where
+import System.Environment
+
+import Text.ParserCombinators.Parsec hiding (spaces)
+import System.Environment
+
+import Control.Monad -- for liftM
+
+data LispVal = Atom String
+             | List [LispVal]
+             | DottedList [LispVal] LispVal
+             | Number Integer
+             | String String
+             | Bool Bool
+
+-- `Remember, each line of a do-block must have the same type, ` ok...
+parseString::Parser LispVal
+parseString = do
+  char '"'
+  x <- many (noneOf "\"")
+  char '"'
+  return $ String x
+
+symbol::Parser Char
+symbol = oneOf "!#$%&|*+-/:<=>?@^_~"
+
+parseAtom::Parser LispVal
+parseAtom = do
+  first <- letter <|> symbol
+  rest <- many (letter <|> digit <|> symbol) -- same as `many $ letter <|> digit <|> symbol` ?
+  let atom = first:rest
+  return $ case atom of
+    "#t" -> Bool True
+    "#f" -> Bool False
+    _ -> Atom atom
+
+parseNumber::Parser LispVal
+parseNumber = liftM (Number . read) $ many1 digit
+
+parseExpr::Parser LispVal
+parseExpr = parseAtom
+    <|> parseString
+    <|> parseNumber
+
+readExper::String -> String
+readExper input = case parse parseExpr "lisp" input of
+  Left err -> "No match:" ++ show err
+  Right _ -> "found value"
+
+-- spaces::Parser ()
+-- spaces = skipMany1 space
+
+-- readExper::String -> String
+-- readExper input = case parse (spaces>>symbol) "lisp" input of
+--   Left err -> "No match: " ++ show err
+--   Right val -> "Found value " ++ show val
+
+main::IO()
+main = do
+  (expr:_) <- getArgs
+  putStrLn(readExper expr)
+
+-- command: ghc -package parsec -o simple_parser data_typed_parser.hs
